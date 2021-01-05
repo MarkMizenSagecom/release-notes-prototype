14cd45aeb4df8c69bda6a4ef85c7a7516897b71f
MarkMizenSagecom <mark.mizen@sage.com>
2021-01-05 17:06:31 +0000

    added multiple releases to demo

--- src/components/Release.js
+++ src/components/Release.js
@@ -1,10 +1,18 @@
 import Heading from "carbon-react/lib/components/heading";
+import styled from "styled-components";
 
 import Part from "./Part";
 
+const Article = styled.article`
+  padding-left: 2em;
+  padding-bottom: 4em;
+  border-left: 2px solid #0073C2;
+`;
+
+
 function Release({ name, parts }) {
   return (
-    <article>
+    <Article>
       <header>
         <Heading title={name} />
       </header>
@@ -12,7 +20,7 @@ function Release({ name, parts }) {
         {parts &&
           parts.map((part, index) => <Part contentPart={part} key={index} />)}
       </main>
-    </article>
+    </Article>
   );
 }
 
